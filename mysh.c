33037e5bd32399b31a34a9f4c58f077b0bc95098
Asmi D. Narsay <adn54@ilab3.cs.rutgers.edu>
2026-04-09 11:34:18 -0400

    outline in comments

---
+++ mysh.c
@@ -0,0 +1,16 @@
+// for the outline:
+// data structure to use:
+    // create a struct that is a linked list/tree like structure that connects the parent process to its children
+        // this struct also contains the unique process id 
+    `   // and a variable that keeps track of if the process was a success ??
+        // and a list of that node's child processes
+        // terminate this list when done with those processes 
+
+    // no need to create a list of parent nodes because only one parent node can exist at a time (mysh)
+        // in the context of this project
+
+// create a temporary list for the tokenizing part bc the tokenizing part just splits up
+// the sentence so we know what command is being called
+// so basically a string 
+
+
