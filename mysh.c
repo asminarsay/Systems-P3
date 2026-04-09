f5c149aecac372ce6b2cfeefe2c583e926577ba0
Asmi D. Narsay <adn54@ilab3.cs.rutgers.edu>
2026-04-09 18:19:28 -0400

    structs

--- mysh.c
+++ mysh.c
@@ -4,6 +4,7 @@
         // this struct also contains the unique process id 
     `   // and a variable that keeps track of if the process was a success ??
         // and a list of that node's child processes
+            // put the list in order of the piping/redirection where the last index is the destination
         // terminate this list when done with those processes 
 
     // no need to create a list of parent nodes because only one parent node can exist at a time (mysh)
@@ -13,4 +14,17 @@
 // the sentence so we know what command is being called
 // so basically a string 
 
+
+typedef struct process{
+    char **env; // input information or string of action
+    pid_t pid; // unique process id
+    int success; // 0 if success 1 if not
+    struct *next; // list of child nodes starts at the next node
+    command_t command; // commands that was entered at this process
+}process_t;
+
+typedef struct command{
+    char **strCommand; // command from terminal
+    // definitely adding more or changing it around
+}command_t;
 
